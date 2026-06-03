3102cd07c8bd7de0b3057e63ecab2ac62e6b243c
Wahab-Al <dev70tec@gmail.com>
2026-06-04 00:43:49 +0200

    feat: configure Tailwind CSS in Vite & create and implement Personal Data form closes #2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-
+import PersonalData from './components/personal-data/PersonalData';
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Praxis Management</h1>
+      <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <h1 className="text-center text-3xl font-extrabold text-gray-900 tracking-tight">
+              Praxis Management System
+            </h1>
+          </div>
+
+        
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-2xl">
+          <PersonalData />
+        </div>
       </div>
     </>
   )
